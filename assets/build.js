6074cf2b263230c6c4ff4db7fcd9fddb4b17c38d
Commandtechno <68407783+Commandtechno@users.noreply.github.com>
2022-01-25 22:48:29 -0600

    fix assets

--- assets/build.js
+++ assets/build.js
@@ -1,24 +1,27 @@
-const { readdirSync, unlinkSync } = require("fs");
+const { mkdirSync, readdirSync, unlinkSync } = require("fs");
 const { resolve } = require("path");
 const { exec } = require("child_process");
 
-readdirSync(resolve(__dirname, "input")).forEach(file => {
-  const input = resolve(__dirname, "input", file);
-  const output = resolve(__dirname, "output", file.slice(0, file.indexOf(".")) + ".webp");
-  console.log(input, output);
-  // try {
-  //   unlinkSync(output);
-  // } catch {}
+const inputDir = resolve(__dirname, "input");
+const outputDir = resolve(__dirname, "output");
 
-  // exec(
-  //   'ffmpeg -i "' +
-  //     input +
-  //     "\" -vf \"scale='min(500,iw)':'min(500,ih)':force_original_aspect_ratio=increase\" -loop 0 \"" +
-  //     output +
-  //     '"',
-  //   error => {
-  //     if (error) return console.log(error);
-  //     console.log(input, "->", output);
-  //   }
-  // );
+try {
+  mkdirSync(outputDir);
+} catch {}
+
+readdirSync(inputDir).forEach(file => {
+  const input = resolve(inputDir, file);
+  const output = resolve(outputDir, file.slice(0, file.indexOf(".")) + ".webp");
+
+  try {
+    unlinkSync(output);
+  } catch {}
+
+  exec(
+    `ffmpeg -i "${input}" -vf "scale='min(500,iw)':'min(500,ih)':force_original_aspect_ratio=increase" -loop 0 "${output}"`,
+    error => {
+      if (error) return console.log(error);
+      console.log(input, "->", output);
+    }
+  );
 });
