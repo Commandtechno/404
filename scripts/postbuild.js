22f71513d7865f6be9d906ae7a4c97e696876fec
Commandtechno <68407783+Commandtechno@users.noreply.github.com>
2022-12-06 20:42:53 -0600

    add new gifs and use yarn!

--- scripts/postbuild.js
+++ scripts/postbuild.js
@@ -1,6 +1,28 @@
-const { resolve } = require("path");
-const { cpSync } = require("fs");
+const { resolve, extname } = require("path");
+const { readdirSync, existsSync, mkdirSync } = require("fs");
+const { execFileSync } = require("child_process");
 
-cpSync(resolve(__dirname, "..", "assets"), resolve(__dirname, "..", "build", "assets"), {
-  recursive: true
-});
+const INPUT_DIR = resolve(__dirname, "..", "assets");
+const OUTPUT_DIR = resolve(__dirname, "..", "build", "assets");
+
+if (!existsSync(OUTPUT_DIR)) {
+  mkdirSync(OUTPUT_DIR);
+}
+
+for (const file of readdirSync(INPUT_DIR)) {
+  const input = resolve(INPUT_DIR, file);
+  const output = resolve(OUTPUT_DIR, file.replace(extname(file), ".webp"));
+
+  if (!existsSync(output)) {
+    console.log(`Processing ${file}...`);
+    execFileSync("ffmpeg", [
+      "-i",
+      input,
+      "-vf",
+      "scale='min(500,iw)':'min(500,ih)':force_original_aspect_ratio=increase",
+      "-loop",
+      "0",
+      output
+    ]);
+  }
+}
