544b52186f6c47f4270df6c89c5781eb7c123b51
Commandtechno <68407783+Commandtechno@users.noreply.github.com>
2023-08-30 00:27:11 -0500

    fix dependencies and font, add hover animation

--- scripts/build.js
+++ scripts/build.js
@@ -1,5 +1,5 @@
 const esbuild = require("esbuild");
-const esbuildHtmlMinify = require("../../esbuild-plugin-minify-html");
+const esbuildHtmlMinify = require("esbuild-plugin-html-minify");
 
 esbuild.build({
   entryPoints: ["src/index.html", "src/index.js", "src/styles.css"],
